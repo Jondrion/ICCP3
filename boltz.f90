ed6b0bec7d5847c0a0c91125fadada80c38398d7
Laurens <laurensjanssen92@gmail.com>
2014-04-24 17:35:09 +0200

    added grid generation and some extra parameters

--- boltz.f90
+++ boltz.f90
@@ -1,11 +1,20 @@
 program Boltz
 
   implicit none
-  real(8) :: lsize, relaxtime, tubewidth, tubelength
+  real(8) :: lsize, relaxtime, tubewidth, tubelength, gridsize
+  real(8), allocatable :: gridarray(:,:,:)
+  integer :: gridtype, n_y, n_x
 
-  call parameters(lsize, relaxtime, tubewidth, tubelength)
+  call parameters(lsize, relaxtime, tubewidth, tubelength, gridsize, gridtype)
   print *, "lsize=",lsize, "relaxtime=",relaxtime, "tubewidth", tubewidth, "tubelength", tubelength
 
+
+!-- Create the grid array
+  n_y = int(tubewidth/(gridsize*SQRT(3._8)/2._8))+1
+  n_x = int(tubelength/gridsize)+1
+
+  allocate(gridarray(n_x,n_y,gridtype))
+  print *,'grid dimensions: ', SIZE(gridarray,1), SIZE(gridarray,2), SIZE(gridarray,3)
   
 end program
 
--- parameters.f90
+++ parameters.f90
@@ -1,11 +1,14 @@
-subroutine parameters(lsize, relaxtime, tubewidth, tubelength)
+subroutine parameters(lsize, relaxtime, tubewidth, tubelength, gridsize, gridtype)
 
-  real(8), intent(out) :: lsize, relaxtime, tubewidth, tubelength
+  real(8), intent(out) :: lsize, relaxtime, tubewidth, tubelength, gridsize
+  integer, intent(out) :: gridtype
   open(10,file="parameters.txt")
   read(10,*) lsize
   read(10,*) relaxtime
   read(10,*) tubewidth
   read(10,*) tubelength
+  read(10,*) gridsize
+  read(10,*) gridtype
   close(10)
 
 end subroutine
