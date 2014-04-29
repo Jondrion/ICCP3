454fab6a4e4a7fd1f79665175211121a98b67dc7
Jonne <J.S.Moeskops@student.tudelft.nl>
2014-04-29 23:08:12 +0200

    unfinished movedensity routine

--- timestep.f90
+++ timestep.f90
@@ -11,4 +11,24 @@ subroutine movedensity(dataarray, x, y)
 	real(8) :: temparray(x,y,7)
 	integer :: i,j,k
 
+	do i=1, x
+		do j=1,y
+			do k=1,7
+				if (k==1) then
+					temparray(i,j,k)=temparray(i,j,k)+dataarray(i,j,k)
+				else if (k==2) then
+					temparray(i,j,k)=temparray(i,j,k)+dataarray(i,j,k)
+				else if (k==3) then
+					temparray(i,j,k)=temparray(i,j,k)+dataarray(i,j,k)
+				else if (k==4) then
+					temparray(i,j,k)=temparray(i,j,k)+dataarray(i,j,k)
+				else if (k==5) then
+					temparray(i,j,k)=temparray(i,j,k)+dataarray(i,j,k)
+				else if (k==6) then
+					temparray(i,j,k)=temparray(i,j,k)+dataarray(i,j,k)
+				end if
+			end do
+		end do
+	end do
+
 end subroutine
