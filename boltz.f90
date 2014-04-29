db9b9be1c988a58731cf070ffc2d4d51be4a854e
Jonne <J.S.Moeskops@student.tudelft.nl>
2014-04-30 00:56:06 +0200

    added disp functionality and movedensity works

--- boltz.f90
+++ boltz.f90
@@ -1,22 +1,33 @@
 program Boltz
+    
+    use dispmodule
 
-  implicit none
-  real(8) :: lsize, relaxtime, tubewidth, tubelength, gridsize
-  real(8), allocatable :: gridarray(:,:,:)
-  integer :: gridtype, n_y, n_x
+    implicit none
+    real(8) :: lsize, relaxtime, tubewidth, tubelength, gridsize
+    real(8), allocatable :: gridarray(:,:,:)
+    integer :: gridtype, n_y, n_x
 
-  call parameters(lsize, relaxtime, tubewidth, tubelength, gridsize, gridtype)
-  print *, "lsize=",lsize, "relaxtime=",relaxtime, "tubewidth", tubewidth, "tubelength", tubelength
+    call parameters(lsize, relaxtime, tubewidth, tubelength, gridsize, gridtype)
+    print *, "lsize=",lsize, "relaxtime=",relaxtime, "tubewidth", tubewidth, "tubelength", tubelength
 
 
 !-- Create the grid array
-  n_y = int(tubewidth/(gridsize*SQRT(3._8)/2._8))+1
-  n_x = int(tubelength/gridsize)+1
+    n_y = int(tubewidth/(gridsize*SQRT(3._8)/2._8))+1
+    n_x = int(tubelength/gridsize)+1
 
-  allocate(gridarray(n_x,n_y,gridtype))
-  print *,'grid dimensions: ', SIZE(gridarray,1), SIZE(gridarray,2), SIZE(gridarray,3)
-  
- ! call timestep(gridarray, n_x, n_y)
+    allocate(gridarray(n_y,n_x,gridtype))
+    print *,'grid dimensions: ', 'x',SIZE(gridarray,2), 'y:',SIZE(gridarray,1), SIZE(gridarray,3)
+
+    gridarray=0
+
+    gridarray(3,3,1:7)=1
+
+    call disp(sum(gridarray,3))
+
+    call timestep(gridarray, n_x, n_y)
+
+    print *,"grid"
+    call disp(sum(gridarray,3))
 
 end program
 
--- timestep.f90
+++ timestep.f90
@@ -1,59 +1,61 @@
 subroutine timestep(dataarray, x, y)
-	integer, intent(in) :: x,y
-	real(8), intent(inout) :: dataarray(x,y,7)
+    integer, intent(in) :: x,y
+    real(8), intent(inout) :: dataarray(y,x,7)
 
-	call movedensity(dataarray,x,y)
+    call movedensity(dataarray,x,y)
 
 
 contains
 
-	subroutine movedensity(dataarray, x, y)
-		real(8), intent(inout) :: dataarray(x,y,7)
-		real(8) :: temparray(x,y,7)
-		integer :: i,j,k
+    subroutine movedensity(dataarray, x, y)
+        integer, intent(in) :: x,y
+        real(8), intent(inout) :: dataarray(y,x,7)
+        real(8) :: temparray(y,x,7)
+        integer :: i,j,k
 
-		do i=2, x-1
-			do j=2,y-1
-				do k=1,7
-					if (mod(j,2)==0)
-						if (k==1) then
-							temparray(i,j,k)=dataarray(i,j,k)
-						else if (k==2) then
-							temparray(i+1,j,k)=dataarray(i,j,k)
-						else if (k==3) then
-							temparray(i+1,j-1,k)=dataarray(i,j,k)
-						else if (k==4) then
-							temparray(i,j-1,k)=dataarray(i,j,k)
-						else if (k==5) then
-							temparray(i-1,j,k)=dataarray(i,j,k)
-						else if (k==6) then
-							temparray(i,j+1,k)=dataarray(i,j,k)
-						else if (k==7) then
-							temparray(i+1,j+1,k)=dataarray(i,j,k)
-						end if
-					else
-						if (k==1) then
-							temparray(i,j,k)=dataarray(i,j,k)
-						else if (k==2) then
-							temparray(i+1,j,k)=dataarray(i,j,k)
-						else if (k==3) then
-							temparray(i,j-1,k)=dataarray(i,j,k)
-						else if (k==4) then
-							temparray(i-1,j-1,k)=dataarray(i,j,k)
-						else if (k==5) then
-							temparray(i-1,j,k)=dataarray(i,j,k)
-						else if (k==6) then
-							temparray(i-1,j+1,k)=dataarray(i,j,k)
-						else if (k==7) then
-							temparray(i,j+1,k)=dataarray(i,j,k)
-						end if
-					end if
-				end do
-			end do
-		end do
-		
-		dataarray=temparray
+        do i=2, x-1
+            do j=2,y-1
+                do k=1,7
+                    if (mod(i,2)==0) then
+                        if (k==1) then
+                            temparray(i,j,k)=dataarray(i,j,k)
+                        else if (k==2) then
+                            temparray(i,j+1,k)=dataarray(i,j,k)
+                        else if (k==3) then
+                            temparray(i-1,j+1,k)=dataarray(i,j,k)
+                        else if (k==4) then
+                            temparray(i-1,j,k)=dataarray(i,j,k)
+                        else if (k==5) then
+                            temparray(i,j-1,k)=dataarray(i,j,k)
+                        else if (k==6) then
+                            temparray(i+1,j,k)=dataarray(i,j,k)
+                        else if (k==7) then
+                            temparray(i+1,j+1,k)=dataarray(i,j,k)
+                        end if
+                    else
+                        if (k==1) then
+                            temparray(i,j,k)=dataarray(i,j,k)
+                        else if (k==2) then
+                            temparray(i,j+1,k)=dataarray(i,j,k)
+                        else if (k==3) then
+                            temparray(i-1,j,k)=dataarray(i,j,k)
+                        else if (k==4) then
+                            temparray(i-1,j-1,k)=dataarray(i,j,k)
+                        else if (k==5) then
+                            temparray(i,j-1,k)=dataarray(i,j,k)
+                        else if (k==6) then
+                            temparray(i+1,j-1,k)=dataarray(i,j,k)
+                        else if (k==7) then
+                            temparray(i+1,j,k)=dataarray(i,j,k)
+                        end if
+                    end if
+                end do
+            end do
+        end do
+        
+        dataarray=temparray
 
-	end subroutine movedensity
+    end subroutine movedensity
 
 end subroutine timestep
+
