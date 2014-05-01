36a25042600a34f4158aa85eae2ac4a89753491a
Jonne <J.S.Moeskops@student.tudelft.nl>
2014-05-01 18:20:28 +0200

    added totaldensity factor in equildensity routine

--- timestep.f90
+++ timestep.f90
@@ -17,7 +17,7 @@ subroutine timestep(dataarray, x, y, pressure_grad, relaxtime)
 
     call add_pressure(dataarray,velocities,x,y,pressure_grad)
 
-    call calculate_equildensity(equildensity,velocities, x, y)
+    call calculate_equildensity(equildensity,dataarray,velocities, x, y)
 
     call relax_density(dataarray,equildensity,x,y,relaxtime)
 
@@ -148,30 +148,35 @@ contains
 
     end subroutine add_pressure
 
-    subroutine calculate_equildensity(equildensity, velocities,x,y)
+    subroutine calculate_equildensity(equildensity, dataarray, velocities,x,y)
 
         use dispmodule
         integer, intent(in) :: x,y
-        real(8),  intent(in) :: velocities(y,x,2)
+        real(8),  intent(in) :: velocities(y,x,2), dataarray(y,x, 7)
         real(8),  intent(out) :: equildensity(y,x,7)
         real(8) :: direction1D(12)
         real(8) :: direction(6,2)
         real(8), parameter :: Pi=4*atan(1d0)
+        real(8) :: totaldensity(y,x)
         integer :: i,j,k
 
         direction1D=[1._8,COS(Pi/3),COS(2*Pi/3),-1._8,COS(4*Pi/3),COS(5*Pi/3), &
              0._8,SIN(Pi/3),SIN(2*Pi/3),0._8,SIN(4*Pi/3),SIN(5*Pi/3)]
         direction=reshape(direction1D,[6,2])
 
+        totaldensity=sum(dataarray,3)
         equildensity=0
+
+        print *,"totaldensity: "
+        call disp(totaldensity)
 
          do i=1,x
             do j=1,y
                 do k=1,7
                     if (k==1) then
-                        equildensity(j,i,k)=1._8/2*(1-2*dot_product(velocities(j,i,1:2),velocities(j,i,1:2)))                        
+                        equildensity(j,i,k)=1._8/2*totaldensity(j,i)*(1-2*dot_product(velocities(j,i,1:2),velocities(j,i,1:2)))                        
                     else
-                        equildensity(j,i,k)=1._8/12*(1+4*dot_product(velocities(j,i,1:2),direction(k-1,1:2)) &
+                        equildensity(j,i,k)=1._8/12*totaldensity(j,i)*(1+4*dot_product(velocities(j,i,1:2),direction(k-1,1:2)) &
                             -2*dot_product(velocities(j,i,1:2),velocities(j,i,1:2)) &
                                 +8*dot_product(velocities(j,i,1:2),direction(k-1,1:2))**2)
                     end if
